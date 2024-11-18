3930df94e7eae2f21b8ccec94ae0aeaa6eb5847e
Dawid Ryłko <dawid.rylko@gmail.com>
2024-11-18 18:52:16 +0100

    feat: about us page

---
+++ example-site/src/pages/o-nas/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Page, Seo, ContactSection, InterestsSection, Animation, Section } from 'gatsby-theme-portfolio-minimal';
+import * as classes from './style.module.css';
+import { useLocalDataSource } from './data';
+
+export default function AboutUsPage() {
+    const { allAboutUsJson } = useLocalDataSource();
+
+    return (
+        <>
+            <Seo title="Tworzymy technologię, która wspiera rozwój biznesu" useTitleTemplate={true} />
+            <Page>
+                <Animation type="fadeUp">
+                    <Section heading="Kim jesteśmy?" anchor="o-nas">
+                        {allAboutUsJson.nodes.map((item, index) => (
+                            <div key={index} className={classes.aboutUsItem}>
+                                <h3 className={classes.aboutUsHeading}>{item.heading}</h3>
+                                <p className={classes.aboutUsContent}>{item.content}</p>
+                            </div>
+                        ))}
+                    </Section>
+                </Animation>
+                <InterestsSection sectionId="oferta" heading="Zobacz w czym możemy Ci pomóc" />
+                <ContactSection sectionId="kontakt" heading="Twoje potrzeby są dla nas priorytetem" />
+            </Page>
+        </>
+    );
+}
