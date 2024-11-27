9c326b42f8e1d8fe68b786fabe98efbaad1b9de5
Dawid Ryłko <dawid.rylko@gmail.com>
2024-11-27 13:26:27 +0100

    chore: notify Google via Indexing API and Bing via URL Submission API

---
+++ .github/scripts/bing.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+
+BASE_URL="https://silesiansolutions.com"
+STATIC_PAGES=("oferta" "realizacje" "o-nas" "blog" "kontakt")
+POSTS_DIR="../../../example-site/content/articles/"
+TMP_DIR="$(pwd)/tmp"
+TMP_FILE="$TMP_DIR/bing.json"
+
+start_time=$(date +%s.%3N)
+
+log_error() {
+  echo "Error: $1"
+  exit 1
+}
+
+# Create a temporary working directory
+create_tmp_directory() {
+  if [ ! -d "$TMP_DIR" ]; then
+    echo "Creating temporary working directory: $TMP_DIR"
+    mkdir "$TMP_DIR" || log_error "Failed to create directory $TMP_DIR"
+  fi
+}
+
+# Construct submission payload
+construct_submission_payload() {
+  local json_content="{\"siteUrl\": \"$BASE_URL\", \"urlList\": [\"$BASE_URL/\","
+
+  for page in "${STATIC_PAGES[@]}"; do
+    json_content+="\"$BASE_URL/$page/\","
+  done
+
+  if [ -d "$POSTS_DIR" ]; then
+    POSTS=("$POSTS_DIR"*/)
+    for ((i=0; i<${#POSTS[@]}; i++)); do
+      POST_DIR="${POSTS[i]}"
+      POST_NAME=$(basename "$POST_DIR")
+      POST_SLUG="${POST_NAME:12}"
+      json_content+="\"$BASE_URL/blog/$POST_SLUG/\","
+    done
+  fi
+
+  if [ -z "$json_content" ]; then
+    log_error "JSON content is empty."
+  fi
+
+  json_content="${json_content%,}]}"
+
+  echo "Constructed JSON Body:"
+  echo "$json_content" | jq '.' > "$TMP_FILE" || log_error "Failed writing to $TMP_FILE"
+  cat "$TMP_FILE"
+  echo
+}
+
+# Check if Bing API key is set
+check_api_key() {
+  if [ -z "${BING_API_KEY:-}" ]; then
+    log_error "BING_API_KEY is not set or is empty. Please set the API key and run the script again."
+  fi
+}
+
+# Submit URLs to Bing search engine
+submit_to_search_engine() {
+  local bing_api_url="https://ssl.bing.com/webmaster/api.svc/json/SubmitUrlbatch?apikey=${BING_API_KEY}"
+
+  echo "Submitting to Bing via URL Submission API..."
+  response=$(curl -s -w "%{http_code}" "$bing_api_url" \
+    -H "Content-Type: application/json; charset=utf-8" \
+    -d "@$TMP_FILE" || log_error "Failed submitting to Bing API.")
+
+  http_code=${response: -3}
+  body=${response::-3}
+
+  echo "HTTP Response Code: $http_code"
+  echo "API Response Body:"
+  echo "$body" | jq '.'
+  echo
+}
+
+# Main execution
+create_tmp_directory
+
+cd "$TMP_DIR" || log_error "Failed to change to temporary directory $TMP_DIR"
+
+check_api_key
+construct_submission_payload
+submit_to_search_engine
+
+end_time=$(date +%s.%3N)
+duration=$(echo "scale=0; ($end_time - $start_time) * 1000 / 1" | bc)
+echo "---------------------------------"
+echo "Script completed in $duration milliseconds."
---
+++ .github/scripts/google.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+
+BASE_URL="https://silesiansolutions.com"
+STATIC_PAGES=("oferta" "realizacje" "o-nas" "blog" "kontakt")
+POSTS_DIR="../../../example-site/content/articles/"
+TMP_DIR="$(pwd)/tmp"
+TMP_FILE="$TMP_DIR/google.csv"
+
+start_time=$(date +%s.%3N)
+
+log_error() {
+  echo "Error: $1"
+  exit 1
+}
+
+# Create a temporary working directory
+create_tmp_directory() {
+  if [ ! -d "$TMP_DIR" ]; then
+    echo "Creating temporary working directory: $TMP_DIR"
+    mkdir "$TMP_DIR" || log_error "Failed to create directory $TMP_DIR"
+  fi
+}
+
+# Download https://github.com/usk81/easyindex-cli
+download_easyindex_cli() {
+  EASYINDEX_CLI="./easyindex-cli"
+  if [ ! -f "$EASYINDEX_CLI" ]; then
+    echo "Fetching easyindex-cli binary..."
+    curl -s -L "https://github.com/usk81/easyindex-cli/releases/download/v1.0.6/easyindex-cli_1.0.6_linux_amd64.tar.gz" | tar xz || log_error "Failed to download and extract easyindex-cli binary."
+    echo
+  fi
+}
+
+# Copy credentials file
+copy_credentials_file() {
+  echo "Using the GOOGLE_JSON_KEY_FILE environment variable..."
+  if [ -z "${GOOGLE_JSON_KEY_FILE:-}" ]; then
+    log_error "GOOGLE_JSON_KEY_FILE is not set or is empty. Exiting."
+  fi
+
+  echo "$GOOGLE_JSON_KEY_FILE" > "$TMP_DIR/credentials.json" || log_error "Failed to write to credentials file $TMP_DIR/credentials.json"
+}
+
+# Construct submission payload
+construct_submission_payload() {
+  echo "\"notification_type\",\"url\"" >> "$TMP_FILE"
+  echo "\"URL_UPDATED\",\"${BASE_URL}/\"" >> "$TMP_FILE"
+
+  for page in "${STATIC_PAGES[@]}"; do
+    echo "\"URL_UPDATED\",\"${BASE_URL}/${page}/\"" >> "$TMP_FILE"
+  done
+
+  if [ -d "$POSTS_DIR" ]; then
+    POSTS=("$POSTS_DIR"*/)
+    for ((i=0; i<${#POSTS[@]}; i++)); do
+      POST_DIR="${POSTS[i]}"
+      POST_NAME=$(basename "$POST_DIR")
+      POST_SLUG="${POST_NAME:12}"
+      echo "\"URL_UPDATED\",\"${BASE_URL}/${POST_SLUG}/\"" >> "$TMP_FILE"
+    done
+  fi
+
+  echo "Constructed CSV file contents:"
+  cat "$TMP_FILE"
+  echo
+}
+
+# Submit URLs to Google search engine
+submit_to_search_engine() {
+  echo "Submitting to Google via Indexing API..."
+  "$EASYINDEX_CLI" google publish --csv "$TMP_FILE" -C "$TMP_DIR/credentials.json" || log_error "An error returned by easyindex-cli. Exiting."
+  echo
+}
+
+# Main execution
+create_tmp_directory
+
+cd "$TMP_DIR" || log_error "Failed to change to temporary directory $TMP_DIR"
+
+download_easyindex_cli
+copy_credentials_file
+construct_submission_payload
+submit_to_search_engine
+
+end_time=$(date +%s.%3N)
+duration=$(echo "scale=0; ($end_time - $start_time) * 1000 / 1" | bc)
+echo "---------------------------------"
+echo "Script completed in $duration milliseconds."
